16a517f4f8936bc8b8ad4fec1245ea69eb90a1fb
Daniel Burke <danieljburke@me.com>
2022-12-18 11:48:58 +1100

    Initial Commit

---
+++ raytracer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  raytracer
+//
+//  Created by Daniel Burke on 18/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
