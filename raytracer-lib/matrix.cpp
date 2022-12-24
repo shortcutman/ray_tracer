063ffd5b5ccff2c8c871896f1394d42c0b2f3bb2
Daniel Burke <danieljburke@me.com>
2022-12-24 11:58:03 +1100

    Matrix multiply operation

--- raytracer-lib/matrix.cpp
+++ raytracer-lib/matrix.cpp
@@ -23,13 +23,39 @@ Matrix<N>::Matrix(BaseMatrix matrix) :
 }
 
 template<unsigned int N>
-double Matrix<N>::at(unsigned int row, unsigned int column) {
+double Matrix<N>::at(unsigned int row, unsigned int column) const {
     return _matrix[row][column];
+}
+
+template<unsigned int N>
+Matrix<N>& Matrix<N>::operator*=(const Matrix<N>& rhs) {
+    auto lhs = *this;
+    
+    for (unsigned int row = 0; row < N; row++) {
+        for (unsigned int column = 0; column < N; column++) {
+            double value = 0.0;
+            
+            for (unsigned int size = 0; size < N; size++) {
+                value += lhs.at(row, size) * rhs.at(size, column);
+            }
+            
+            this->_matrix[row][column] = value;
+        }
+    }
+    
+    return *this;
 }
 
 template<unsigned int N>
 bool Matrix<N>::operator==(const Matrix<N>& rhs) const {
     return std::equal(_matrix.begin(), _matrix.end(), rhs._matrix.begin());
+}
+
+template<unsigned int N>
+Matrix<N> operator*(const Matrix<N>& lhs, const Matrix<N>& rhs) {
+    auto copy = lhs;
+    copy *= rhs;
+    return copy;
 }
 
 template class rtlib::Matrix<2>;
--- raytracer-lib/matrix.hpp
+++ raytracer-lib/matrix.hpp
@@ -25,10 +25,19 @@ public:
     Matrix(BaseMatrix matrix);
     ~Matrix() {}
     
-    double at(unsigned int row, unsigned int column);
+    double at(unsigned int row, unsigned int column) const;
+    
+    Matrix<N>& operator*=(const Matrix<N>& rhs);
     
     bool operator==(const Matrix<N>& rhs) const;
 };
+
+template<unsigned int N>
+Matrix<N> operator*(const Matrix<N>& lhs, const Matrix<N>& rhs) {
+    auto copy = lhs;
+    copy *= rhs;
+    return copy;
+}
 
 typedef Matrix<2> Matrix2x2;
 typedef Matrix<3> Matrix3x3;
