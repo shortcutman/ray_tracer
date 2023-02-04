645ec66efc03a0348b249a2dbf10f2f28146eaaa
Daniel Burke <danieljburke@me.com>
2023-02-04 15:23:53 +1100

    Render routine incorporating lighting

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -18,20 +18,29 @@ using namespace rtlib;
 int main(int argc, const char * argv[]) {
     
     auto viewOrigin = create_point(0.0, 0.0, -2.0);
+    Light light(create_point(-10.0, 10.0, -10.0), Colour(1.0, 1.0, 1.0));
     Sphere s;
+    auto m = s.material();
+    m._colour = Colour(1.0, 0.2, 1.0);
+    s.setMaterial(m);
     
-    Canvas c(256, 256);
+    Canvas c(1024, 1024);
     for (unsigned int x = 0; x < c.width(); x++) {
         for (unsigned int y = 0; y < c.height(); y++) {
-            double xRay = -1.0 + static_cast<double>(x) * (2.0 / 256.0);
-            double yRay = -1.0 + static_cast<double>(y) * (2.0 / 256.0);
+            double xRay = -1.0 + static_cast<double>(x) * (2.0 / c.width());
+            double yRay = -1.0 + static_cast<double>(y) * (2.0 / c.height());
             
             Ray r(viewOrigin, create_vector(xRay, yRay, 1.0));
             auto hits = s.intersects(r);
             auto hit = rtlib::hit(hits);
             
             if (hit) {
-                c.write_pixel(x, y, Colour(1.0, 0.0, 0.0));
+                auto hitPoint = r.positionAt(hit->t);
+                auto litColour = light.lightPoint(s.material(),
+                                                  hitPoint,
+                                                  -r.direction().normalised(),
+                                                  s.normalAt(hitPoint));
+                c.write_pixel(x, y, litColour);
             }
         }
     }
