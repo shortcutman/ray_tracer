6228fa2d980ef5ac5f7622f1f21bf722ab791e09
Daniel Burke <danieljburke@me.com>
2023-02-12 15:10:24 +1100

    Draw as left-handed coordinate system

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, const char * argv[]) {
     for (unsigned int x = 0; x < c.width(); x++) {
         for (unsigned int y = 0; y < c.height(); y++) {
             double xRay = -1.0 + static_cast<double>(x) * (2.0 / c.width());
-            double yRay = -1.0 + static_cast<double>(y) * (2.0 / c.height());
+            double yRay = 1.0 - static_cast<double>(y) * (2.0 / c.height());
             
             Ray r(viewOrigin, create_vector(xRay, yRay, 1.0));
             auto hits = s.intersects(r);
