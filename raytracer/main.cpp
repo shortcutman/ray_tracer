723b785706b2f03bf52225d798259c38e3219dfe
Daniel Burke <danieljburke@me.com>
2023-02-18 16:25:05 +1100

    Written to use new world and camera objects.

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -5,45 +5,32 @@
 //  Created by Daniel Burke on 18/12/2022.
 //
 
+#include "camera.hpp"
 #include "canvas.hpp"
-#include "object.hpp"
-#include "ray.hpp"
-#include "tuple.hpp"
+#include "transformations.hpp"
+#include "world.hpp"
 
 #include <chrono>
 #include <iostream>
 #include <fstream>
+#include <numbers>
 
 using namespace rtlib;
 
 int main(int argc, const char * argv[]) {
     auto start = std::chrono::high_resolution_clock::now();
     
-    auto viewOrigin = create_point(0.0, 0.0, -2.0);
-    Light light(create_point(-10.0, 10.0, -10.0), Colour(1.0, 1.0, 1.0));
-    Sphere s;
-    auto m = s.material();
-    m._colour = Colour(1.0, 0.2, 1.0);
-    s.setMaterial(m);
+    auto canvas = Canvas(1024, 1024);
+    auto world = World::defaultWorld();
+    auto camera = Camera(canvas.width(), canvas.height(), std::numbers::pi / 2.0);
+    auto viewTransform = rtlib::viewTransform(create_point(0.0, 0.0, -5.0), create_point(0.0, 0.0, 0.0), create_vector(0.0, 1.0, 0.0));
+    camera.setTransform(viewTransform);
     
-    Canvas c(1024, 1024);
-    for (unsigned int x = 0; x < c.width(); x++) {
-        for (unsigned int y = 0; y < c.height(); y++) {
-            double xRay = -1.0 + static_cast<double>(x) * (2.0 / c.width());
-            double yRay = 1.0 - static_cast<double>(y) * (2.0 / c.height());
-            
-            Ray r(viewOrigin, create_vector(xRay, yRay, 1.0));
-            auto hits = s.intersects(r);
-            auto hit = rtlib::hit(hits);
-            
-            if (hit) {
-                auto hitPoint = r.positionAt(hit->t);
-                auto litColour = light.lightPoint(s.material(),
-                                                  hitPoint,
-                                                  -r.direction().normalised(),
-                                                  s.normalAt(hitPoint));
-                c.write_pixel(x, y, litColour);
-            }
+    for (unsigned int x = 0; x < canvas.width(); x++) {
+        for (unsigned int y = 0; y < canvas.height(); y++) {
+            auto ray = camera.rayForPixel(x, y);
+            auto colour = world.colourAt(ray);
+            canvas.write_pixel(x, y, colour);
         }
     }
     
@@ -58,7 +45,7 @@ int main(int argc, const char * argv[]) {
         return 1;
     }
     
-    file << c;
+    file << canvas;
     file.close();
     
     return 0;
